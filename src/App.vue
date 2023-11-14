ad767e760710b61565db4c7b865f163ffc879064
Dung <dunghq2110@gmail.com>
2023-11-14 20:04:19 +0700

    init src

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  <RouterView />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+</style>
